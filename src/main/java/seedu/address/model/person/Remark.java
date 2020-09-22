4568221dd4534257e3cee66346966c38db13d3e4
StopTakingAllTheNames <ong.yinggao@gmail.com>
2020-09-22 15:38:58 +0800

    Fix coding style violations
    * ParserUtil: removed unused import.
    * Remark: added JavaDoc comment for constructor.

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a new Remark object.
+     *
+     * @param remark Remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
